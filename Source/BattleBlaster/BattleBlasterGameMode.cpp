f48d58a003473ccbd440b57c8fc9889006bfe767
ElBronze <juanchosapardo@gmail.com>
2025-10-10 23:43:14 -0500

    While loop

--- Source/BattleBlaster/BattleBlasterGameMode.cpp
+++ Source/BattleBlaster/BattleBlasterGameMode.cpp
@@ -2,4 +2,47 @@
 
 
 #include "BattleBlasterGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tower.h"
 
+void ABattleBlasterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	TArray<AActor*> Towers;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATower::StaticClass(), Towers);
+	TowerCount = Towers.Num();
+	UE_LOG(LogTemp, Warning, TEXT("Tower count: %d"), TowerCount);
+
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (PlayerPawn)
+	{
+		Tank = Cast<ATank>(PlayerPawn);
+		if (!Tank)	// Tank == nullptr
+		{
+			UE_LOG(LogTemp, Error, TEXT("Player pawn is not of type ATank"));
+		}
+	}
+
+	int32 LoopIndex = 0;
+	while (LoopIndex < TowerCount)
+	{
+
+		AActor* TowerActor = Towers[LoopIndex];
+		if (TowerActor)
+		{
+			ATower* Tower = Cast<ATower>(TowerActor);
+			if (Tower && Tank)
+			{
+				Tower->Tank = Tank;
+
+				UE_LOG(LogTemp, Warning, TEXT("The name of the tower is %s"), *Tower->GetActorNameOrLabel());
+			}
+		}
+		
+		LoopIndex++;
+
+	}
+}
